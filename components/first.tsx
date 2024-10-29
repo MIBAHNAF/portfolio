78b8bbba1291c7c51ca54a87cef7178ecdafc73e
Mir Ahnaf Ali <mirahnafali717@gmail.com>
2024-10-29 14:43:24 -0400

    Normalize all line endings to LF

---
+++ components/first.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+      <h1 className="pb-20 pt-36">First</h1>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+export default First
